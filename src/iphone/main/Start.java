338fbe128bd6a38a762f2920dc564d1fec7c6d94
LuFelix <luciano@tiweb.app.br>
2023-10-23 18:22:44 -0300

    Testes Executados

--- src/iphone/main/Start.java
+++ src/iphone/main/Start.java
@@ -1,14 +1,17 @@
 package iphone.main;
 
+import iphone.implementacao.IPhone;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Start {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-
+        IPhone ip = new IPhone();
+        System.out.println("Olá Bem vindo ao IPhone!");
+        ip.selecionarMusica();
+        ip.aumentarVolume();
 
     }
 }
